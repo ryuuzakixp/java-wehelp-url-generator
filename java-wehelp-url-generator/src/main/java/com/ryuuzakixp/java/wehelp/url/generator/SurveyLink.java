ae6826f29048cc2dea099c442d0e2bce26ab061d
danielzamora <daniel.e.zamoras@gmail.com>
2023-12-07 21:24:12 -0300

    test url encode

--- java-wehelp-url-generator/src/main/java/com/ryuuzakixp/java/wehelp/url/generator/SurveyLink.java
+++ java-wehelp-url-generator/src/main/java/com/ryuuzakixp/java/wehelp/url/generator/SurveyLink.java
@@ -1,23 +1,49 @@
 package com.ryuuzakixp.java.wehelp.url.generator;
 
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.codec.digest.HmacAlgorithms;
+import org.apache.commons.codec.digest.HmacUtils;
+import org.json.JSONObject;
 
 public class SurveyLink {
     private static final String BASE_URL = "https://app.wehelpsoftware.com/survey_persons/link";
     private String queryParams;
 
+    private SurveyLink(Map<String, Object> data, String encryptKey) {
 
-    private SurveyLink(Map<String, Object> data) {
-        this.queryParams = "?access_token=";
+        Map<String, Object> headerMap = new HashMap<>();
+        headerMap.put("alg", "HS256");
+        headerMap.put("typ", "JWT");
+
+        JSONObject jsonHeader = new JSONObject(headerMap);
+        JSONObject jsonData = new JSONObject(data);
+
+        String playload = jsonData.toString();
+        String header = jsonHeader.toString();
+
+        //String signature = new HmacUtils(HmacAlgorithms.HMAC_SHA_256, encryptKey).hmacHex(header + playload);
+        String signature = new HmacUtils(HmacAlgorithms.HMAC_SHA_256, encryptKey).hmacHex(header + playload);
+        
+        String accessToken = base64UrlEncode(header) + '.' + base64UrlEncode(playload) + '.'
+                + base64UrlEncode(signature);
+
+        this.queryParams = "?access_token=" + accessToken;
     }
 
-    public static String generate(Map<String, Object> data) {
-        SurveyLink instance = new SurveyLink(data);
+    public static String generate(Map<String, Object> data, String encryptKey) {
+        SurveyLink instance = new SurveyLink(data, encryptKey);
         return instance.getUrl();
     }
 
     private String getUrl() {
         return BASE_URL + queryParams;
+    }
+
+    private String base64UrlEncode(String input) {
+        String inputBase64 = Base64.getUrlEncoder().encodeToString(input.getBytes());
+        return inputBase64.replace("+", "_").replace("/", "-");
     }
 }
--- java-wehelp-url-generator/src/test/java/com/ryuuzakixp/java/wehelp/url/generator/SurveyLinkTest.java
+++ java-wehelp-url-generator/src/test/java/com/ryuuzakixp/java/wehelp/url/generator/SurveyLinkTest.java
@@ -1,5 +1,6 @@
 package com.ryuuzakixp.java.wehelp.url.generator;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -13,11 +14,29 @@ public class SurveyLinkTest {
     @Test
     public void generateSurveyLink_WhenDataIsValid_ReturnString() {
         Map<String, Object> data = new HashMap<>();
-        data.put("code", "123");
-        
-        String url = SurveyLink.generate(data);
+        data.put("code", "bc6dcb454033a89fdb2d576ade5dd561");
+        data.put("experience_id", "");
+        data.put("experience_date", "2023-12-07 11:00:00");
+        data.put("company_unit_code", "BBLWRI");
 
+        Map<String, Object> personData = new HashMap<>();
+        personData.put("name", "John Doe");
+        personData.put("internal_code", "001");
+        personData.put("type", "CUSTOMER");
+        personData.put("company_unit_code", "BBLWRI");
+
+        data.put("person", personData);
+
+        String url = SurveyLink.generate(data, "953ef82aaa021b40dfa9d09ffc793e3cf2af58ca");
+        System.out.println(url);
         assertNotNull(url);
         assertTrue(url.startsWith("https://app.wehelpsoftware.com/survey_persons/link?access_token="));
+    }
+
+    @Test
+    public void testReplace() {
+        String inputBase64 = "bc6dcb454033a8+9fdb2d57+/6ade5/dd5+61";
+        String result = inputBase64.replace("+", "_").replace("/", "-");
+        assertEquals("bc6dcb454033a8_9fdb2d57_-6ade5-dd5_61", result);
     }
 }
